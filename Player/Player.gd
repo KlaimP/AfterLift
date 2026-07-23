636968dd526fdfe248a459045b3a186381b101ba
Klaim <arhik2.0@gmail.com>
2026-07-23 19:33:18 +0300

    Я хз, как его еще улучшить можно

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,22 +7,24 @@ extends CharacterBody2D
 @export var dash_speed: float = 600.0;
 
 # Здоровье и выносливость игрока
-@export var max_health: int = 100;
-var current_health: int = max_health;
+@export var max_health: float = 100.0;
+var current_health: float = max_health;
 
 @export var max_stamina: int = 100;
 var current_stamina: int = max_stamina;
 
-# Длительность и перезарядка рывка
+# Длительность и перезарядка дэш-атаки, а также длительность неуязвимости от пуль
 @export var dash_duration: float = 0.2;
 @export var dash_cooldown: float = 0.3;
+@export var bullet_invincibility_duration: float = 0.5;
 
 @onready var sprite: Sprite2D = $Sprite2D
 
 # Приватные переменные для управления состоянием игрока
 var is_dash: bool = false;
 var dash_direction: Vector2 = Vector2.ZERO;
-var is_invincible: bool = false;
+var is_dash_invincible: bool = false;
+var is_bullet_invincible: bool = false;
 var can_dash: bool = true;
 
 func _physics_process(delta):
@@ -37,12 +39,13 @@ func _physics_process(delta):
 	elif velocity.x < 0:
 		target_rotation = deg_to_rad(-4);
 
-
 	sprite.rotation = lerp(sprite.rotation, target_rotation, 12 * delta)
 
+	# Обработка дэша
 	if Input.is_action_just_pressed("dash"):
 		dash(input_vector);
 	
+	# Обработка движения игрока
 	if is_dash:
 		velocity = dash_direction.normalized() * dash_speed;
 		sprite.rotation = lerp(sprite.rotation, dash_direction.x * deg_to_rad(20), 20 * delta);
@@ -53,6 +56,7 @@ func _physics_process(delta):
 
 	move_and_slide();
 
+# Функция для выполнения дэша игроком
 func dash(direction: Vector2) -> void:
 	if is_dash or not can_dash:
 		return;
@@ -60,16 +64,17 @@ func dash(direction: Vector2) -> void:
 	if direction == Vector2.ZERO:
 		return;
 	
+	# Честно, я хз нахуя мне столько переменных
 	can_dash = false;
 	is_dash = true;
-	is_invincible = true;
+	is_dash_invincible = true;
 	dash_direction = direction.normalized();
 
 	print("Dashing in direction: ", dash_direction);
 	await get_tree().create_timer(dash_duration).timeout;
 
 	is_dash = false;
-	is_invincible = false;
+	is_dash_invincible = false;
 
 	await get_tree().create_timer(dash_cooldown).timeout;
 
@@ -81,14 +86,20 @@ func dash(direction: Vector2) -> void:
 	#else:
 	#	print("Not enough stamina to dash.")
 
+# Функция для получения урона игроком и сделал еще кадры неуязвимости от пуль, чтобы игрок не получал урон сразу
 func take_damage(amount: float) -> void:
-	if is_invincible:
+	if is_dash_invincible or is_bullet_invincible:
 		return;
-
 	current_health -= amount;
 
 	if current_health <= 0:
 		die();
 
+	# Сделал так, чтобы игрок не получал урон сразу после получения урона от пули, а был неуязвим на некоторое время
+	is_bullet_invincible = true;
+	await get_tree().create_timer(bullet_invincibility_duration).timeout;
+	is_bullet_invincible = false;
+
 func die() -> void:
+	# TODO: Добавить анимацию смерти и обработку конца игры
 	queue_free();
